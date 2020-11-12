fca2e67c5a55dc959387d54fd9d5b5fe1da97cce
Codi <74140844+CodideFaria@users.noreply.github.com>
2020-11-12 09:31:44 +0200

    Add files via upload

---
+++ Web App/readme.txt
@@ -0,0 +1,15 @@
+In order to be able to open the web application you need to have a compatible browser to open it.
+Such as;
+Google
+Firefox
+Safari
+Internet Explorer
+Opera
+Microsoft Edge
+Etc
+
+To start the web app double click the Chrome HTML Document labelled "ToDoList".
+Choose your preferred browser to open it.
+You should see your browser opening up and a page appearing on the screen.
+
+You have successfully connected to the website.
---
+++ Web App/script/script.js
@@ -0,0 +1,178 @@
+var taskInput = document.getElementById("new-task"); //new-task
+var dueDate = document.getElementById("due-date");//due-date
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
+
+//New Task List Item
+var createNewTaskElement = function(taskString) {
+  //Create List Item
+  var listItem = document.createElement("li");
+  //input (checkbox)
+  var checkBox = document.createElement("input"); // checkbox
+  //label
+  var label = document.createElement("label");
+  //input (text)
+  var editInput = document.createElement("input"); // text
+  //button.edit
+  var editButton = document.createElement("button");
+  //button.delete
+  var deleteButton = document.createElement("button");
+  
+  //Each element needs modifying
+  
+  checkBox.type = "checkbox";
+  editInput.type = "text";
+  
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
+  
+  label.innerText = taskString;
+  
+  //Each element needs appending
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+}
+
+//Add a new task
+var addTask = function() {
+  console.log("Add task...");
+  //Create a new list item with the text from #new-task:
+  var listItem = createNewTaskElement(taskInput.value + "\n" + "Due for " + dueDate.value);
+  //Append listItem to incompleteTasksHolder
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+  
+  taskInput.value = "";
+}
+
+//Edit an existing task
+var editTask = function() {
+  console.log("Edit task...");
+
+  var listItem = this.parentNode;
+  
+  var editInput = listItem.querySelector("input[type=text");
+  var label = listItem.querySelector("label");
+  
+  var containsClass = listItem.classList.contains("editMode");
+  
+  //if the class of the parent is .editMode
+  if(containsClass) {
+    //Switch from .editMode
+    //label text become the input's value
+    label.innerText = editInput.value;
+  } else {
+    //Switch to .editMode
+    //input value becomes the label's text
+    editInput.value = label.innerText;
+  }
+  
+  //Toggle .editMode on the list item
+  listItem.classList.toggle("editMode");
+  
+}
+
+//Delete an existing task
+var deleteTask = function() {
+  console.log("Delete task...");
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
+  
+  //Remove the parent list item from the ul
+  ul.removeChild(listItem);
+  
+}
+
+//Mark a task as complete
+var taskCompleted = function() {
+  console.log("Task complete...");
+  //Append the task list item to the #completed-tasks
+  var listItem = this.parentNode;
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
+}
+
+//Mark a task as incomplete
+var taskIncomplete = function() {
+  console.log("Task incomplete...");
+  //Append the task list item to the #incomplete-tasks
+  var listItem = this.parentNode;
+  incompleteTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+  console.log("Bind list item events");
+  //select taskListItem's children
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
+  
+  //bind editTask to edit button
+  editButton.onclick = editTask;
+  
+  //bind deleteTask to delete button
+  deleteButton.onclick = deleteTask;
+  
+  //bind checkBoxEventHandler to checkbox
+  checkBox.onchange = checkBoxEventHandler;
+}
+
+//Set the click handler to the addTask function
+addButton.addEventListener("click", addTask);
+
+//cycle over incompleteTasksHolder ul list items
+for(var i = 0; i < incompleteTasksHolder.children.length; i++) {
+  //bind events to list item's children (taskCompleted)
+  bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+
+//cycle over completedTasksHolder ul list items
+for(var i = 0; i < completedTasksHolder.children.length; i++) {
+  //bind events to list item's children (taskIncomplete)
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
+
+localStorage.setItem("Inctasks", taskIncomplete);
+localStorage.setItem("Comptasks", taskCompleted);
+
+function sortList() {
+  	var list, i, switching, b, shouldSwitch;
+  	list = document.getElementById("incomplete-tasks");
+  	switching = true;
+  		/* Make a loop that will continue until
+  		no switching has been done: */
+  	while (switching) {
+    		// start by saying: no switching is done:
+    	switching = false;
+    	b = list.getElementsByTagName("LI");
+    		// Loop through all list-items:
+    	for (i = 0; i < (b.length - 1); i++) {
+      			// start by saying there should be no switching:
+      		shouldSwitch = false;
+      			/* check if the next item should
+      			switch place with the current item: */
+      		if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        			/* if next item is alphabetically
+        			lower than current item, mark as a switch
+        			and break the loop: */
+        		shouldSwitch = true;
+        		break;
+      		}
+    	}
+    	if (shouldSwitch) {
+      			/* If a switch has been marked, make the switch
+      			and mark the switch as done: */
+      		b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      		switching = true;
+   		}
+  	}
+}
